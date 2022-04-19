6563cc745bb2644788e6030b5b68fe755d9a6566
Padmajase <padmajapawar7@gmail.com>
2022-04-18 23:40:46 -0700

    Added : Employee Wage program UC6

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,44 +2,50 @@ import java.util.Random;
 
 class EmployeeWage {
 
-	static int wagePerHr = 20;
-	static int fullTimeHr = 8;
-	static int partTimeHr = 8;
+	static int wagePerHr = 20; 
+	static int workingHr = 8;
 	static int dayPerMonth = 20;
+	static int HrPerMonth = 20;
+	static int totalWorkingHr  = 0 ;
 
-	public static void main(String args[]) {
+	/** calculating wage till working hour reached for a month **/
 
-	System.out.println("Welcome !");
-
-	Random rand = new Random();
-	int isEmployee = rand.nextInt(3);	
-	System.out.println(isEmployee);
-	
-	/**calculating wages for a month**/
-	switch (isEmployee) {
-	case 1 :
+	public static void main(String args[])
 	{
-		System.out.println("Employee is full time Present");
-		System.out.println("Daily full time Employee Wage = " +(wagePerHr * fullTimeHr * dayPerMonth));
-		
-	}
-	break;
-	case 2 :
-	{
-		System.out.println("Employee is part time Present");
-		System.out.println("Daily part time Employee Wage = " +(wagePerHr * partTimeHr * dayPerMonth));
-	}	
-	break;
-	case 0 :
-	{
-		System.out.println("Employee is Absent");
-		System.out.println("Daily Employee Wage = 0 " );
-	}
-	break;
-	default :
-		System.out.println("Invalid");
-	break;
+		System.out.println("Welcome !");
 
-	}
+		while( totalWorkingHr <= 100 && dayPerMonth <=20 ) {
+			Random rand = new Random();
+			int isEmployee = rand.nextInt(3);	
+			
+			switch (isEmployee) {
+			case 1 :
+			{
+				System.out.println("Employee is full time Present");
+				int wage1 = (wagePerHr * workingHr );
+				System.out.println("Daily full time Employee Wage = " + wage1);
+			}
+			break;
+			case 2 :
+			{
+				System.out.println("Employee is part time Present");
+				int wage2 = (wagePerHr * workingHr );
+				System.out.println("Daily part time Employee Wage = " + wage2);
+			}	
+			break;
+			case 0 :
+			{
+				System.out.println("Employee is Absent");
+				System.out.println("Daily Employee Wage = 0 " );
+			}
+			break;
+			default :
+			System.out.println("Invalid");
+			break;
+
+			} 
+			totalWorkingHr = totalWorkingHr + workingHr ;
+			System.out.println(totalWorkingHr );
+		}
 	}
 }
