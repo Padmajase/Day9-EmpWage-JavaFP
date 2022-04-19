ede1951cbe339ac8706887f0bb9d3348debb217f
Padmajase <padmajapawar7@gmail.com>
2022-04-18 23:44:21 -0700

    Added : Employee Wage program UC8

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,60 +1,29 @@
-/** solving using class variable and method**/
+/** calculating Wages for multiple company**/
+
+import java.util.Scanner;
 import java.util.Random;
 
-class EmployeeWage {
-
+class EmployeeWage 
+{
 	public static void main(String args[])
 	{
 		System.out.println("Welcome !");
-		ComputeWage.computeWage();
+
+		System.out.println("Enter the wagePerHour, workingHour, workingDays of compony for which  u want to calculate the wage");
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();	
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		Wage.calculateEmpWage(a, b, c);
 	}
 }
-
-class ComputeWage
+class Wage
 {
-	
-	static int wagePerHr = 20; 
-	static int workingHr = 8;
-	static int dayPerMonth = 20;
-	static int HrPerMonth = 20;
-	static int totalWorkingHr  = 0 ;
-
-	static void computeWage()
-	{
-		System.out.println("Welcome !");
-
-		while( totalWorkingHr <= 100 && dayPerMonth <=20 ) {
-			Random rand = new Random();
-			int isEmployee = rand.nextInt(3);	
-			
-			switch (isEmployee) {
-			case 1 :
-			{
-				System.out.println("Employee is full time Present");
-				int wage1 = (wagePerHr * workingHr );
-				System.out.println("Daily full time Employee Wage = " + wage1);
-			}
-			break;
-			case 2 :
-			{
-				System.out.println("Employee is part time Present");
-				int wage2 = (wagePerHr * workingHr );
-				System.out.println("Daily part time Employee Wage = " + wage2);
-			}	
-			break;
-			case 0 :
-			{
-				System.out.println("Employee is Absent");
-				System.out.println("Daily Employee Wage = 0 " );
-			}
-			break;
-			default :
-			System.out.println("Invalid");
-			break;
-
-			} 
-			totalWorkingHr = totalWorkingHr + workingHr ;
-			System.out.println(totalWorkingHr );
-		}
-	}
+	static void calculateEmpWage(int wagePerHour, int workingHour, int workingDays)
+	{	
+		int employeeWage = (wagePerHour * workingHour * workingDays);
+		System.out.println(" Employee Wage for the company = " + employeeWage);
+		
+	}	
 }
